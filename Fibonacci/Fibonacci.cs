a03c546c0be37fd7fc2726c1c7b6a097cbfcb36b
Anchita Arora <anchita.arora@asos.com>
2022-06-09 12:30:52 +0100

    fibonacci kata using 3 rules of tdd

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        public int GenerateFibonacciNumberForPosition(int pos)
+        {
+            if (pos == 0 || pos == 1)
+            {
+                return pos;
+            }
+
+            return GenerateFibonacciNumberForPosition(pos - 1) + GenerateFibonacciNumberForPosition(pos - 2);
+
+        }
+    }
+}
---
+++ Fibonacci/FibonacciTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Fibonacci
+{
+    using NUnit.Framework;
+    class FibonacciTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(2, 1)]
+        [TestCase(3, 2)]
+        [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        [TestCase(7, 13)]
+
+        public void Should_return_0_for_position_0(int position, int expectedFibonacci)
+        {
+            var fibonacci = new Fibonacci();
+            int fib = fibonacci.GenerateFibonacciNumberForPosition(position);
+            Assert.That(fib, Is.EqualTo(expectedFibonacci));
+        }
+    }
+}
